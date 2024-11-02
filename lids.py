79d3baa540b067eeb1dd6b3c3abde13fa219c6bd
Abusaid <brodyaga8556@gmail.com>
2024-11-02 08:05:18 +0500

    end

--- lids.py
+++ lids.py
@@ -5,7 +5,7 @@ import datetime
 
 bot = telebot.TeleBot("7103426824:AAHYGRa3bfg3oID7mZVDW09ny_LaPo7Lszg", parse_mode=None)
 def sender(text):
-    bot.send_message('-1', text)
+    bot.send_message('-1001752114157', text)
 
 
 def botforlid(request, next_page=None):
@@ -14,7 +14,6 @@ def botforlid(request, next_page=None):
     template = 'index.html'  # Шаблон по умолчанию
     if next_page == 'index2':
         template = 'index2.html'
-    # sender(txt)
-    print(txt)
+    sender(txt)
     return render( request, template)
 
