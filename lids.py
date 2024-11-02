85164804713889e1b4dd1c5c095f2084403eabac
Abusaid <brodyaga8556@gmail.com>
2024-11-02 08:33:25 +0500

    end0.2

--- lids.py
+++ lids.py
@@ -4,16 +4,17 @@ import datetime
 
 
 bot = telebot.TeleBot("7103426824:AAHYGRa3bfg3oID7mZVDW09ny_LaPo7Lszg", parse_mode=None)
+print(bot.message_handler)
 def sender(text):
-    bot.send_message('-1001752114157', text)
+    bot.send_message('-4599062176', text)
 
 
 def botforlid(request, next_page=None):
-    
-    txt = f"Ism : {request.POST.get('name')}\nRaqam : {request.POST.get('email')}\n"
-    template = 'index.html'  # Шаблон по умолчанию
+    template = 'index.html'  # Шаблон по умолчанию  
     if next_page == 'index2':
         template = 'index2.html'
-    sender(txt)
+        if request.POST.get('name')  and request.POST.get('email')  : 
+            txt = f"Ism : {request.POST.get('name')}\nRaqam : {request.POST.get('email')}\n"
+            sender(txt)
     return render( request, template)
 
